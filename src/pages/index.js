51355635444b186b19c1025a19357b1b3a585384
Divine Orji <dpkreativ@gmail.com>
2022-05-25 06:59:44 +0100

    import and set up Image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
+import Image from '../components/Image';
 
 export default function Home() {
   // === CLOUDINARY MEDIA ===
@@ -24,6 +25,13 @@ export default function Home() {
   return (
     <div style={{ width: '100%', maxWidth: '425px', margin: '0 auto' }}>
       <h1>Home</h1>
+
+      <section style={{ display: 'grid', gap: '2rem' }}>
+        {cldImages &&
+          cldImages.map((imageNode, index) => (
+            <Image key={`${index}-cld`} image={imageNode} />
+          ))}
+      </section>
     </div>
   );
 }
